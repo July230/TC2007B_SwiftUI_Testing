bfb0964013b7888650d3bb4c6f3f76f9e73cda2e
Ian Julián Estrada Castro <A01352823@tec.mx>
2024-11-05 17:49:29 -0600

    Innitial commit

---
+++ test_app_live/test_app_live/test_app_liveApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_app_liveApp.swift
+//  test_app_live
+//
+//  Created by Ian Julian Estrada Castro on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct test_app_liveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test_app_live/test_app_liveTests/test_app_liveTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_app_liveTests.swift
+//  test_app_liveTests
+//
+//  Created by Ian Julian Estrada Castro on 05/11/24.
+//
+
+import Testing
+@testable import test_app_live
+
+struct test_app_liveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
